4aa717f4bc212de5b7fbc67017ab91c736c72a78
glove.tux <alexzsk@outlook.com>
2024-09-07 10:10:33 -0400

    Update README.nix

--- README.nix
+++ README.nix
@@ -2,3 +2,7 @@
   oo|  alexzsk.github.io    
  /`'\  this website is inspired by kisslinux     
 (\_;/)
+
+so what website i tested the html code to make sure i can make chanages fine without the site breaking
+
+i used codepen.io to test the html code
